72ef97fc920f232bf34cfa14373968be15ca5193
Thibault Leroy <thibleroy@thibaults-macbook-pro.home>
2021-11-04 12:40:39 +0100

    update scripts

--- get_ios_apps_versions.py
+++ get_ios_apps_versions.py
@@ -4,6 +4,7 @@ from authlib.jose import jwt
 KEY_ID = os.environ['APPLE_KEY_ID']
 ISSUER_ID = os.environ['APPLE_ISSUER_ID']
 PATH_TO_KEY = os.environ['APPLE_PATH_TO_KEY']
+
 EXPIRATION_TIME = int(round(time.time() + (20.0 * 60.0))) # 20 minutes timestamp (do not inscrease!)
 
 with open(PATH_TO_KEY, 'r') as f:
@@ -21,17 +22,14 @@ payload = {
     "aud": "appstoreconnect-v1"
 }
 
-# Create the JWT
 token = jwt.encode(header, payload, PRIVATE_KEY)
 
-# API Request
 JWT = 'Bearer ' + token.decode()
 URL = 'https://api.appstoreconnect.apple.com/v1/apps'
 HEAD = {'Authorization': JWT}
 
 r = requests.get(URL, params={'limit': 200}, headers=HEAD)
 
-# Write the response in a pretty printed JSON file
 with open('output.json', 'w') as out:
     out.write(json.dumps(r.json(), indent=4))
 
